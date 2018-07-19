e24713ca4305973313b966b5c55152c43d932339
shubham <shubham99bisht@gmail.com>
2018-07-19 15:59:12 +0530

    Add files via upload

---
+++ mnist_test.py
@@ -0,0 +1,13 @@
+
+def model():
+    from keras.models import model_from_json
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
+
+
+    return loaded_model
